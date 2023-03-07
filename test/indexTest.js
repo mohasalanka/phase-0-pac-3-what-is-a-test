c2992eb812dfd44db40c2a178ad1f3fb4312c7b7
Mohamed <mohasalanka2020@gmail.com>
2023-03-07 17:42:31 +0300

    completed asignment

--- test/indexTest.js
+++ test/indexTest.js
@@ -29,9 +29,8 @@ describe('what-is-a-test', () => {
 // The code below ensures that students who are using CodeGrade will get credit 
 // for the code-along in Canvas; you can disregard it.
 
-describe('', () => {
-  describe('', () => {
-    it('', () => {
+  describe('message', () => {
+    it('gives name and height', () => {
       return true;
     })
   })
